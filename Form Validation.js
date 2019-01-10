86e71641830532ccf562b5714539cd6d0a0eeae7
ntuchol <ntuchol@bu.edu>
2019-01-10 11:03:55 -0600

    Create Form Validation.js

---
+++ Form Validation.js
@@ -0,0 +1,14 @@
+//HTML
+<form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
+Name: <input type="text" name="fname">
+<input type="submit" value="Submit">
+</form>
+
+//JS
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
